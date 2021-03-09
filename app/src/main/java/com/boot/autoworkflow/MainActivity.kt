4ce4a56d611b8a15eb52acb041cb509553e34afb
wasin.p <wasin.p@go-jek.com>
2021-03-09 23:15:00 +0700

    add compose,rx,coroutine require dependencies

--- app/src/main/java/com/boot/autoworkflow/MainActivity.kt
+++ app/src/main/java/com/boot/autoworkflow/MainActivity.kt
@@ -22,8 +22,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.boot.autoworkflow.ui.theme.AppTheme
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,15 +41,19 @@ class MainActivity : AppCompatActivity() {
     setContent {
       AppTheme {
         // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) { Greeting("Android") }
+        Surface(color = MaterialTheme.colors.background) {
+          var text by remember { mutableStateOf("Initial") }
+          LaunchedEffect(Unit) { testSubscribe { text += "\n$it" } }
+          Greeting(text)
+        }
       }
     }
   }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Greeting(text: String) {
+  Text(text)
 }
 
 // Start building your app here!
@@ -59,4 +72,27 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
   AppTheme(darkTheme = true) { MyApp() }
+}
+
+fun testSubscribe(showText: (String) -> Unit) {
+  val getCompletable: () -> Completable = {
+    Completable.fromCallable { Thread.sleep(500) }.subscribeOn(Schedulers.io())
+  }
+  val api =
+      Single.fromCallable {
+        Thread.sleep(500)
+        1
+      }
+
+  val dispoable =
+      api
+          .flatMapCompletable {
+            Completable.merge(
+                listOf(getCompletable(), getCompletable(), getCompletable(), getCompletable()))
+          }
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribeOn(Schedulers.io())
+          .subscribe { showText("On subscribe: this will print later") }
+
+  showText("Last line of function: this will print first")
 }
--- buildSrc/buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/buildSrc/src/main/kotlin/Deps.kt
@@ -1,18 +1,58 @@
 import org.gradle.api.JavaVersion
 
 object Deps {
-    object Core {
-        const val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradle}"
-        const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-        val java = JavaVersion.VERSION_1_8
-    }
-    object Spotless {
-        private const val version = "5.10.2"
-        const val classPath = "com.diffplug.spotless:spotless-plugin-gradle:${version}"
-        const val pluginId = "com.diffplug.spotless"
-    }
+  object Core {
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradle}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    val java = JavaVersion.VERSION_1_8
+  }
+  object Spotless {
+    private const val version = "5.10.2"
+    const val classPath = "com.diffplug.spotless:spotless-plugin-gradle:${version}"
+    const val pluginId = "com.diffplug.spotless"
+  }
 
-    object Compose {
-//        const val version = "1.0.0-beta01"
-    }
+  object Rx {
+    private const val rxJavaVersion = "2.2.21"
+    private const val rxAndroidVersion = "2.1.1"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${rxAndroidVersion}"
+    const val rxJava = "io.reactivex.rxjava2:rxjava:${rxJavaVersion}"
+  }
+
+  object Coroutine {
+    private const val version = "1.4.3"
+    const val coroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${version}"
+    const val coroutineAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${version}"
+    const val coroutineRx = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:${version}"
+    const val coroutineReactive = "org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${version}"
+  }
+
+  object Compose {
+
+    const val activity = "androidx.activity:activity-compose:1.3.0-alpha03"
+    const val ui = "androidx.compose.ui:ui:${Versions.compose}"
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    const val foundation = "androidx.compose.foundation:foundation:${Versions.compose}"
+    // Material Design
+    const val material = "androidx.compose.material:material:${Versions.compose}"
+    const val materialIcon = "androidx.compose.material:material-icons-core:${Versions.compose}"
+    const val materialIconsExt = "androidx.compose.material:material-icons-extended:${Versions.compose}"
+    // Tooling support (Previews, etc.)
+    const val uiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+
+    const val animation = "androidx.compose.animation:animation:${Versions.compose}"
+    const val animationCore = "androidx.compose.animation:animation-core:${Versions.compose}"
+
+    const val uiTesting = "androidx.compose.ui:ui-test-junit4::${Versions.compose}"
+
+
+
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha03"
+    const val uiGeometry = "androidx.compose.ui:ui-geometry:${Versions.compose}"
+    const val uiGraphic = "androidx.compose.ui:ui-graphics:${Versions.compose}"
+    const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.compose}"
+    const val uiText = "androidx.compose.ui:ui-text:${Versions.compose}"
+    const val uiUtils = "androidx.compose.ui:ui-util:${Versions.compose}"
+    const val uiViewBinding = "androidx.compose.ui:ui-viewbinding:${Versions.compose}"
+  }
 }
