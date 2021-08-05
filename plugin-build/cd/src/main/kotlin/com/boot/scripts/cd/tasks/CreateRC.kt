e703590c11b10194d83d282f951af8ff30e20179
wasin.p <wasin.p@go-jek.com>
2021-08-06 00:03:42 +0700

    test rc tag

--- plugin-build/cd/src/main/kotlin/com/boot/scripts/cd/tasks/CreateRC.kt
+++ plugin-build/cd/src/main/kotlin/com/boot/scripts/cd/tasks/CreateRC.kt
@@ -1,6 +1,5 @@
 package com.boot.scripts.cd.tasks
 
-import com.lordcodes.turtle.ShellLocation
 import com.lordcodes.turtle.shellRun
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
@@ -13,12 +12,15 @@ open class CreateRC : DefaultTask() {
     //		val shellLocation = ShellLocation.HOME.resolve(project.rootDir)
     //		val output = shellRun("git", listOf("status"), shellLocation)
     //		println(output)
-//    println(ShellLocation.CURRENT_WORKING)
+    //    println(ShellLocation.CURRENT_WORKING)
 
     shellRun {
       git.gitCommand(listOf("fetch", "--prune", "--tags"))
-//      val tag = git.gitCommand(listOf("tag", "-l", "-n1"))
-      val tag = git.gitCommand(listOf("describe", "--abbrev=0", "--tags"))
+      //      val tag = git.gitCommand(listOf("tag", "-l", "-n1"))
+      //      val tag = git.gitCommand(listOf("describe", "--abbrev=0", "--tags"))
+  
+      
+      val tag = git.gitCommand("describe --long --first-parent --match \"v[0-9]*\"".split(" "))
       val version = tag.removePrefix("v")
       println(version)
       tag
