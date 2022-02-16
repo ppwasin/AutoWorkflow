89c8190a738f1e3bb508d9c3ac37308cd2527935
pp.wasin <pp.wasin@gmail.com>
2022-02-16 13:33:38 +0700

    platform: use android splash screen library and change the logo

---
+++ app/src/main/java/com/boot/autoworkflow/App.kt
@@ -0,0 +1,5 @@
+package com.boot.autoworkflow
+
+import android.app.Application
+
+class App: Application()
--- app/src/main/java/com/boot/autoworkflow/ui/screen/MainActivity.kt
+++ app/src/main/java/com/boot/autoworkflow/ui/screen/MainActivity.kt
@@ -16,17 +16,18 @@
 package com.boot.autoworkflow.ui.screen
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.boot.entrypoint.ui.EntrypointScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
+    installSplashScreen()
     setContent { EntrypointScreen() }
   }
 }
