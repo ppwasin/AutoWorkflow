3f14d588434a44d241bf87474ca8a182014fab5e
wpassornpako <wasin.passornpakorn@agoda.com>
2022-05-24 11:12:56 +0700

    [refactor] extract to modifier gesture

---
+++ platform/components/src/main/kotlin/com/boot/components/gesture/Pointergesture.kt
@@ -0,0 +1,31 @@
+package com.boot.components.gesture
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+
+fun Modifier.pressGesture(scaleSize: Float = 1.2f, onClick: () -> Unit): Modifier = composed {
+  var selected by remember { mutableStateOf(false) }
+  val scale by animateFloatAsState(if (!selected) scaleSize else 1f)
+  graphicsLayer {
+      scaleX = scale
+      scaleY = scale
+    }
+    .pointerInput(Unit) {
+      detectTapGestures(
+        onPress = {
+          selected = true
+          if (tryAwaitRelease()) onClick()
+          selected = false
+        }
+      )
+    }
+}
