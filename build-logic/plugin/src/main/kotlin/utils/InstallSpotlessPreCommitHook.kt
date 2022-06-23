8dfd9a8682a171a18d9da00a3864d38569229c94
ppwasin <pp.wasin@gmail.com>
2022-06-23 11:56:29 +0700

    fix: cannot run spotless hook install due to configuration cache

--- build-logic/plugin/src/main/kotlin/utils/InstallSpotlessPreCommitHook.kt
+++ build-logic/plugin/src/main/kotlin/utils/InstallSpotlessPreCommitHook.kt
@@ -1,24 +1,28 @@
 package utils
 
 import java.io.File
+import javax.inject.Inject
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-open class InstallSpotlessPreCommitHook : DefaultTask() {
+abstract class InstallSpotlessPreCommitHook @Inject constructor(
+  private val rootDirPath: String
+) : DefaultTask() {
   @TaskAction
   fun installHook() {
-    val gitHooksDirectory = File("${project.rootDir}/.git/hooks/")
+    val gitHooksDirectory = File("${rootDirPath}/.git/hooks/")
     if (!gitHooksDirectory.exists()) {
       gitHooksDirectory.mkdirs()
     }
-    File("${project.rootDir}/.git/hooks", "pre-commit")
-        .writeText(
-            """
+    File("${rootDirPath}/.git/hooks", "pre-commit")
+      .writeText(
+        """
                 #!/bin/bash
                 echo "Running spotless check"
                 ./gradlew spotlessApply
                 git add .
-                """.trimIndent())
+                """.trimIndent()
+      )
     Runtime.getRuntime().exec("chmod +x .git/hooks/pre-commit")
   }
 }
